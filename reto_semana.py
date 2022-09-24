28e09cb38cb2c493c7d2d73698f970bfcd173227
mariohgu <mario@handong.ac.kr>
2022-09-25 02:13:16 +0900

    reto semana

--- reto_semana.py
+++ reto_semana.py
@@ -11,16 +11,72 @@
  */
  '''
  
-from datetime import date
-import math
+# from datetime import date
+# import math
 
-def date_dif(date_play:date.fromisoformat)-> str:
-    date_tof=date.fromisoformat('2023-05-12')
-    diferencia = date_tof-date_play
-    dif_anio=math.floor(diferencia.days/365)
-    dif_days = round(diferencia.days%365)
-    print (f'Faltan {dif_anio} años con {dif_days} dias')
+# def date_dif(date_play:date.fromisoformat)-> str:
+#     date_tof=date.fromisoformat('2023-05-12')
+#     diferencia = date_tof-date_play
+#     dif_anio=math.floor(diferencia.days/365)
+#     dif_days = round(diferencia.days%365)
+#     print (f'Faltan {dif_anio} años con {dif_days} dias')
 
-date_dif(date.fromisoformat('2021-08-10'))
+# date_dif(date.fromisoformat('2021-08-10'))
+
+
+
+'''
+/*
+ * Enunciado: Crea un programa se encargue de transformar un número binario
+ * a decimal sin utilizar funciones propias del lenguaje que 
+ * lo hagan directamente.
+ */'''
+ 
+
+
+def a_dec(num:str):
+    s = 0
+    num = list(num)
+    for i in range(len(num)):
+        j = num.pop()
+        if j == '1':
+            s = s + pow(2,i)
+    print(s)
+#     list_num =list(num)
+#     suma = 0
+    
+#     for valor in range(len(list_num)):
+#         numero = list_num.pop()
+#         if numero =='1':
+#             suma = suma + pow(2,valor)
+
+#     print(suma)
+
+
+
+
+
+def nombre(namex:str):
+    nuevo = namex[:namex.find('@')]
+    nuevo = nuevo+'@ceu.es'
     
     
+    print(nuevo)
+    
+
+
+
+def run():
+    
+   # n = input("Dame tu nombre: ")
+    #nombre(n)
+    a_dec('01110')
+
+    # a = [1,2,4,5]
+    # b = a.copy()
+    # b = [7,8,9,7]
+    # print (a,b)
+
+    
+ 
+if __name__ == '__main__':run()
